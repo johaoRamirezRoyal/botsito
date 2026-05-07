374c60c5af30fc87eb8d54b194afa2fabdb6c544
Johao Ramirez <hernando.ramirez@royalschool.edu.co>
2026-05-07 11:40:53 -0500

    Array de correos

--- data.py
+++ data.py
@@ -1,3 +1,28 @@
 import pandas as pd
 
 df = pd.read_csv('correos.csv')
+
+df_correos = df.rename(columns={
+    'First Name [Required]': 'primer_nombre',
+    'Last Name [Required]': 'apellido',
+    'Email Address [Required]': 'email'
+})
+
+def devolver_correos(df):
+    df = df.copy()
+
+    df['correo_generado'] = (
+    df['primer_nombre']
+    .astype(str)
+    .str.strip()
+    .str.lower()
+    + '.' + df['apellido'] 
+    .astype(str)
+    .str.strip()
+    .str.lower()
+    .str.replace(' ', '')
+    + '@royalschool.edu.co'
+    )
+
+    correos = df['correo_generado'].tolist()
+    return correos[:500]
