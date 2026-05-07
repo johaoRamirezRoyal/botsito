41797cd348303fb1573b733b9534269e0c58d812
Johao Ramirez <hernando.ramirez@royalschool.edu.co>
2026-05-07 14:47:32 -0500

    Update función de correos

--- data.py
+++ data.py
@@ -26,3 +26,25 @@ def devolver_correos():
 
     correos = df['correo_generado'].tolist()
     return correos[:500]
+
+def eliminar_correos_enviados(correos_enviados):
+    df = df_correos.copy()
+
+    df['correo_generado'] = (
+    df['primer_nombre']
+    .astype(str)
+    .str.strip()
+    .str.lower()
+    + '.' + df['apellido'] 
+    .astype(str)
+    .str.strip()
+    .str.lower()
+    .str.replace(' ', '')
+    + '@royalschool.edu.co'
+    )
+
+    df_filtrado = df[~df['correo_generado'].isin(correos_enviados)]
+
+    correos_restantes = df_filtrado['correo_generado'].tolist()
+    
+    return correos_restantes
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from setup import inputFormIntersection
-from data import devolver_correos
+from data import devolver_correos, eliminar_correos_enviados
 
 def main():
     numero_correos = 0
     correos = devolver_correos()
-
+    correos_enviado = []
     for correo in correos:
-        if numero_correos >= 1:
+        if numero_correos >= 10:
             print("Se han enviado 10 correos, deteniendo el bot.")
             break
         inputFormIntersection(correo)
+        correos_enviado.append(correo)
         numero_correos += 1
         print(f"✅ Correo enviado: {correo} | Total enviados: {numero_correos}")
-
+        correos_restantes = eliminar_correos_enviados(correos_enviado)
+        print(f"Correos restantes por enviar: {len(correos_restantes)}")
 main()
